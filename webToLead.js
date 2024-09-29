ae2f7b9f90cedd9b4df13684722b9cc507e87f77
nikhilKumarAvusula <nikhilkumaravusula@gmail.com>
2024-09-29 21:30:39 +0530

    alert

--- webToLead.js
+++ webToLead.js
@@ -1,12 +1,28 @@
+let captachChecked = false;
 function handleSubmit(){
-
+if(captachChecked){
     let inputValue = document.querySelector(".inputValue");
     let outPutValue = document.querySelector(".outputValue");
 
     let formattedDate = new Date(inputValue.value).toLocaleString("en-IN");
   
     outPutValue.value = formattedDate;
-
+} else{
+    alert("Please fill recaptcha");
+}
+    
 }
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+function timestamp(){
+     var response = document.getElementById("g-recaptcha-response");
+      if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+     }
+     } 
+     setInterval(timestamp, 500);
+     
+function callbackSuccess(){
+    captachChecked= true;
+}
